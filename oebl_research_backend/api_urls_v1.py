ba27d1c2a30d2082727dd61529e311e5feb85669
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2022-03-11 15:46:02 +0100

    resolves #40 [skip ci]

--- oebl_research_backend/api_urls_v1.py
+++ oebl_research_backend/api_urls_v1.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework import routers
 
 from .api_views import LemmaResearchView, ListViewset
+from .autocompletes import ProfessionGroupAutocomplete
 
 app_name = "oebl_research_backend"
 
@@ -11,3 +12,10 @@ router.register(r"listresearch", ListViewset)
 router.register(r"lemmaresearch", LemmaResearchView)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('autocompletes/professiongroup/',
+    ProfessionGroupAutocomplete.as_view(),
+    name='profession-group-autocomplete'
+    )
+]

8c1ad09447c3b555684febe6522841592685266b
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-06-29 18:14:04 +0200

    Added Serializers For All New Models
    Tags: [skip ci]

---
+++ oebl_editor/serializers.py
@@ -0,0 +1,34 @@
+from oebl_editor.models import LemmaArticle, LemmaArticleVersion, UserArticleAssignment, UserArticlePermission
+from rest_framework import serializers
+
+
+class LemmaArticleSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = LemmaArticle
+        fields = ('issue_lemma', 'published', 'current_version', )
+        
+
+class LemmaArticleVersionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = LemmaArticleVersion
+        fields = ('lemma_article', 'markup', 'date_created', 'date_modified', )
+        read_only_fields = ('date_created', 'date_modified', )
+        
+
+userArticleEdityTypeMappingFields = ('lemma_article', 'user', 'edit_type', )
+        
+        
+class UserArticlePermissionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = UserArticlePermission
+        fields = userArticleEdityTypeMappingFields
+
+
+class UserArticleAssignmentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = UserArticleAssignment
+        fields = userArticleEdityTypeMappingFields

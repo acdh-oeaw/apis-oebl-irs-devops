11d76616a87ed873044adaf9492a0941ce74833a
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-06-30 13:19:55 +0200

    Nested Extract Mark Test
    Tags: [skip ci]

--- oebl_editor/tests/unittests/test_queries.py
+++ oebl_editor/tests/unittests/test_queries.py
@@ -2,10 +2,14 @@
 Test oebl_editor.queries
 """
 
+from typing import TYPE_CHECKING
 from unittest import TestCase
 
 from oebl_editor.queries import extract_marks_flat
 
+if TYPE_CHECKING:
+    from oebl_editor.markup import AbstractBaseNode
+    
 
 class TestExtractMarksFlat(TestCase):
     
@@ -18,4 +22,24 @@ class TestExtractMarksFlat(TestCase):
         node = {'type': 'someparenttype', 'marks': marks}
         result = list(extract_marks_flat(node, node_type))
         self.assertEqual(marks, result)
+        
+        
+    def test_nested(self):
+        node_type = 'example_node_type'
+        mark1, mark2, mark3 = marks = [{'type': node_type, 'text': 'mark1'}, {'type': node_type, 'text': 'mark2'}, {'type': node_type, 'text': 'mark3'}]
+        parent_node: 'AbstractBaseNode' = {
+            'type': 'paragraph',
+            'marks': [mark1, ],
+            'content': [
+                {
+                    'type': 'somechildnode',
+                    'marks': [
+                        mark2, mark3, 
+                    ]
+                },
+            ]
+        }
+        result = list(extract_marks_flat(parent_node, node_type))
+        self.assertEqual(marks, result)
+        
         

89a5ee52bd19d6443ab771c65a1316efb8d6489d
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-06-30 13:27:00 +0200

    Test: Mixed Mark Types
    Tags: [skip ci]

--- oebl_editor/tests/unittests/test_queries.py
+++ oebl_editor/tests/unittests/test_queries.py
@@ -3,7 +3,7 @@ Test oebl_editor.queries
 """
 
 from typing import TYPE_CHECKING
-from unittest import TestCase
+from unittest import TestCase, result
 
 from oebl_editor.queries import extract_marks_flat
 
@@ -41,5 +41,21 @@ class TestExtractMarksFlat(TestCase):
         }
         result = list(extract_marks_flat(parent_node, node_type))
         self.assertEqual(marks, result)
+        
+    
+    def test_mixed_mark_types(self):
+        mark_type_1 = 'mark_type_1'
+        mark_type_2 = 'mark_type_2'
+        
+        mark1, mark2, mark3 = marks = [{'type': mark_type_1, 'text': 'mark1'}, {'type': mark_type_2, 'text': 'mark2'}, {'type': mark_type_1, 'text': 'mark3'}]
+        node: 'AbstractBaseNode' = {'type': 'parentnode', 'marks': marks}
+        
+        result1 = list(extract_marks_flat(node, mark_type_1))
+        self.assertEqual(result1, [mark1, mark3])
+        
+        result2 = list(extract_marks_flat(node, mark_type_2))
+        self.assertEqual(result2, [mark2, ])
+
+         
         
         

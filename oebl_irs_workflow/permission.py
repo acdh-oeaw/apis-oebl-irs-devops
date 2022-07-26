e108db9d0c2edc6f49a207bcaa5199fab478a8d8
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-07-26 17:09:40 +0200

    Object Permission For DELETE IssueLemma.editor
    Working tests for DeleteTestCase in oebl_irs_workflow/tests/test_editor_assignments.py
    
    Tags: [skip ci]

--- oebl_irs_workflow/permission.py
+++ oebl_irs_workflow/permission.py
@@ -4,6 +4,7 @@ from rest_framework import permissions
 if TYPE_CHECKING:
     from rest_framework.request import Request
     from django.contrib.auth.models import User
+    from oebl_irs_workflow.models import IssueLemma
     from oebl_irs_workflow.api_views import IssueLemmaViewset
 
 
@@ -25,5 +26,19 @@ class IssueLemmaEditorAssignmentPermissions(permissions.BasePermission):
 
         return False
 
+
+    def has_object_permission(self, request: 'Request', view: 'IssueLemmaViewset', obj: 'IssueLemma'):
+        
+        user: 'User' = request.user
+
+        # Super user can do it all
+        if user.is_superuser:
+            return True
+
+        # Only super users can delete an editor assignment
+        if request.method == 'DELETE' and obj.editor is not None:
+            return False
+        
+        return True
 
     

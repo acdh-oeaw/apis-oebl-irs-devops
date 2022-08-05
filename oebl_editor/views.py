ec960087e33dee921e520a127319cd629878220c
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-08-05 11:24:37 +0200

    Adding lemma article As Query Param To Version Endpoint
    Tags: [skip ci]

--- oebl_editor/views.py
+++ oebl_editor/views.py
@@ -2,6 +2,8 @@ from oebl_editor.permissions import AbstractReadOnlyPermissionViewSetMixin, Lemm
 from oebl_editor.queries import create_get_query_set_method_filtered_by_user
 from rest_framework import viewsets
 from rest_framework import permissions
+from drf_spectacular.utils import extend_schema_view, extend_schema
+from drf_spectacular.openapi import OpenApiParameter
 from oebl_editor.serializers import LemmaArticleSerializer, LemmaArticleVersionSerializer
 from oebl_editor.models import LemmaArticle, LemmaArticleVersion
 
@@ -13,6 +15,13 @@ class LemmaArticleViewSet(AbstractReadOnlyPermissionViewSetMixin, viewsets.Model
     get_queryset = create_get_query_set_method_filtered_by_user(LemmaArticle, 'pk')
         
     
+@extend_schema_view(
+    list = extend_schema(
+        parameters=[
+            OpenApiParameter('lemma_article', type=int),
+        ],
+    ),
+)
 class LemmaArticleVersionViewSet(viewsets.ModelViewSet):
     
     serializer_class = LemmaArticleVersionSerializer

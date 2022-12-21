73ba0c2246ce6ff4bc2ece6dc2a5f32b89cbfa3b
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2022-12-21 12:48:39 +0100

    allow argument for specifying ip and port for runserver command

--- start_dev.sh
+++ start_dev.sh
@@ -6,4 +6,4 @@ python manage.py migrate --settings=apis.settings.dev
 mkdir /tmp/celery
 supervisord -c celery_config/celery_dev.conf
 supervisorctl -c celery_config/celery_dev.conf start all
-python manage.py runserver
+python manage.py runserver ${1}

8a989152decfc3278f8a89027917a43e2de2547b
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2023-07-17 17:51:07 +0200

    chore: adds custom token auth class to change header keyword to "Bearer"
    fixes #65

--- apis/settings/base.py
+++ apis/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from typing import Dict, Any
+from rest_framework.authentication import TokenAuthentication
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(
@@ -104,6 +105,10 @@ SPECTACULAR_SETTINGS: Dict[str, Any] = {
 
 CRISPY_TEMPLATE_PACK = "bootstrap3"
 
+class IRSTokenAuthentication(TokenAuthentication):
+    keyword = "Bearer"
+
+
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 50,
@@ -114,6 +119,7 @@ REST_FRAMEWORK = {
         # use IsAuthenticated for every logged in user to have global edit rights
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        IRSTokenAuthentication,
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
